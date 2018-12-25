9262fc57b6df8aa1d76afa226a701d0d77d42458
Ricardo Rocha <ricardoproblaz@gmail.com>
2018-12-25 15:12:19 -0700

    first commit

---
+++ app/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { FooterContainer } from './styles';
+
+function Footer() {
+  return (
+    <FooterContainer>
+      EasyCredit
+    </FooterContainer>
+  );
+}
+
+export default Footer;
